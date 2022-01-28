0b1ee364ed22a993b66c5defaafd1e4dd4424e92
pjgowtham <pjgowtham@gmail.com>
2022-01-29 04:10:39 +0530

    sm8350-common: Convert tree to oplus

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter lemonade lemonadep,$(TARGET_DEVICE)),)
+ifneq ($(filter martini RMX2202 RMX3360,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
